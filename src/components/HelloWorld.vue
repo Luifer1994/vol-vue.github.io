5d461301093e5c5341982ad8e33f5e8dc181c16b
Luifer <almendralesluifer@gmail.com>
2021-10-15 10:18:26 -0500

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,7 @@
             <svg class="icon icon-xs me-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
             </svg>
-            Nueva Orden
+            Nueva Transacción
         </a>
         <div class="btn-group ms-2 ms-lg-3">
             <button type="button" class="btn btn-sm btn-outline-gray-600">Exportar</button>
